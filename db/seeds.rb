f32155bf363bc218995a31996d1c7fe2c9b61bb4
Apprentice <apprentice@devbootcamp.com>
2013-06-21 16:27:14 -0700

    uncommented seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 module TasksImporter
 
-  # 3.times {List.create(name: Faker::Name.name)}
-  # list_ids = List.pluck(:id)
+  3.times {List.create(name: Faker::Name.name)}
+  list_ids = List.pluck(:id)
 
-  # 30.times {Task.create(item: Faker::Company.bs, list_id: list_ids.sample)}
+  30.times {Task.create(item: Faker::Company.bs, list_id: list_ids.sample)}
 
 end
